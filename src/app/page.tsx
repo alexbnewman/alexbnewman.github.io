de2cd88210ffdec9d14c633a32ba07b64d1ea281
alexbnewman <alex02new@outlook.com>
2024-01-01 18:46:24 -0500

    Snowfall attempt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,26 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Nav from "./components/nav";
+import Script from "next/script";
 
 export default function Home() {
   return (
     <>
-      <main className={styles.main}>
-        <div>
-          <h1>Alex Newman</h1>
-        </div>
-        <div className={styles.center}>
-          <p>
-            Hi, my name is Alex and I'm a software engineer attending the{" "}
-            <b>University of Michigan.</b>
-          </p>
-        </div>
-        <Nav />
-      </main>
+      <div className={styles.snow}>
+        <main className={styles.main}>
+          <script src="snowfall.ts"></script>
+          <div>
+            <h1>Alex Newman</h1>
+          </div>
+          <div className={styles.center}>
+            <p>
+              Hi, my name is Alex and I'm a software engineer attending the{" "}
+              <b>University of Michigan.</b>
+            </p>
+          </div>
+          <Nav />
+        </main>
+      </div>
     </>
   );
 }
