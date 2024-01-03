3447472ec8ff07c376ca0e0250f7caf4e1333791
alexbnewman <alex02new@outlook.com>
2024-01-03 18:18:35 -0500

    Small update for background

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
+import styles from "./page.module.css";
+
 export default function About() {
   return (
-    <main>
-        <div>
-            <h1>About Me</h1>
-        </div>
-
+    <main className={styles.main}>
+      <div>
+        <h1>About Me</h1>
+      </div>
     </main>
-  )}
+  );
+}
