cf7e7588967064848e10c14422ca6f70149f20a6
alexbnewman <alex02new@outlook.com>
2024-01-05 18:23:37 -0500

    Styling updates.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function About() {
   return (
@@ -6,22 +7,51 @@ export default function About() {
       <div>
         <h1>About Me</h1>
       </div>
-      <div>
+      <div className={styles.container}>
+        <Image
+          src="/um_logo.png"
+          className={styles.img}
+          width={350}
+          height={350}
+          alt="U-M logo."
+        />
+        <div className={styles.verticalLine}></div>
         <p>
-          I am a student at the University of Michigan, double majoring in
-          computer science and sociology. I am particularly interested in
-          full-stack development, and most recently was employed as a web
-          developer for U-M's College of Engineering.
+          I am a student at the University of Michigan (graduating in May of
+          2025), double majoring in computer science and sociology. I am
+          particularly interested in full-stack development, and most recently
+          was employed as a web developer for U-M's College of Engineering.
         </p>
+      </div>
+      <div className={styles.container}>
         <p>
           I am currently working as Lead Developer for a UMich student
           organization called Void Tech. I manage a team of six developers, and
           our organization works to assist and solve issues for the U-M student
           community.
         </p>
+        <div className={styles.verticalLine}></div>
+        <Image
+          src="/void_logo.jpg"
+          className={styles.img}
+          width={350}
+          height={350}
+          alt="Void logo."
+        />
+      </div>
+      <div className={styles.container}>
+        <Image
+          src="/void_logo.jpg"
+          className={styles.img}
+          width={325}
+          height={325}
+          alt="U-M logo."
+        />
+        <div className={styles.verticalLine}></div>
         <p>
-          I love to rock climb, snowboard, read, and discover new places to camp
-          out with my laptop.
+          I love to rock climb 🧗, snowboard 🏂, read 📚, and discover new
+          places (cafés, museums, libraries, etc.!) to camp out with my laptop
+          💻.
         </p>
       </div>
     </main>
--- src/app/components/homeNav.tsx
+++ src/app/components/homeNav.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import Link from "next/link";
-import Image from "next/image";
 import NavLink from "./navLink";
 import styles from "./nav.module.css";
 
