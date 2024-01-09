6f73307e9e58c074ae8c0a28b8a8e42208f61aa5
alexbnewman <alex02new@outlook.com>
2024-01-09 16:39:07 -0500

    Added contact images and updated favicon

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,34 @@
+import styles from "./page.module.css";
+import Image from "next/image";
+
 export default function Contact() {
   return (
-    <main>
+    <main className={styles.main}>
       <div>
         <h1>Contact Me</h1>
+      </div>
+      <div>
+        <Image
+          src="/email.png"
+          className={styles.img}
+          width={350}
+          height={350}
+          alt="Email logo."
+        />
+        <Image
+          src="/linkedin_logo.png"
+          className={styles.img}
+          width={350}
+          height={350}
+          alt="LinkedIn logo."
+        />
+        <Image
+          src="/github.png"
+          className={styles.img}
+          width={350}
+          height={350}
+          alt="Github logo."
+        />
       </div>
     </main>
   );
